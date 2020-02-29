f342fe53c27d201f51ea07385088fa180a9b6ef4
prafull kumar <Prafulla@circles.life>
2020-02-29 13:45:31 +0800

    feat: init xcode project with iOS support 12.0

---
+++ BlockChain/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  BlockChain
+//
+//  Created by prafull kumar on 29/2/20.
+//  Copyright © 2020 prafull kumar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = UINavigationController.init(rootViewController: BCRootViewController.init())
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ BlockChain/BCRootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BCRootViewController.swift
+//  BlockChain
+//
+//  Created by prafull kumar on 29/2/20.
+//  Copyright © 2020 prafull kumar. All rights reserved.
+//
+
+import UIKit
+
+class BCRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
